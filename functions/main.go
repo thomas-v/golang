fddc74eeb912f1795611267cbe8f9c1ba76cbab1
tvitiello <tvitiello@corp.free.fr>
2020-07-24 14:09:00 +0200

    fonctions

---
+++ functions/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println(add(5,5))
+	fmt.Println(getNames())
+	fmt.Println(split(17))
+}
+
+func add(x, y int) int{
+	return x + y;
+}
+
+func getNames() (string, string, string) {
+	return "Thomas", "Andre", "Arnaud"
+}
+
+func split(sum int) (x, y int){
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
